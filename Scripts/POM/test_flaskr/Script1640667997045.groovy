5f9cb53d8a4e9023033f1e658db0c9930dab4f74
kazurayam <kazuaki.urayama@gmail.com>
2021-12-29 13:01:06 +0900

    refactored Test Cases/POM/test_flaskr: introduce the step0 function to modularize the test case script

--- Scripts/POM/test_flaskr/Script1640667997045.groovy
+++ Scripts/POM/test_flaskr/Script1640667997045.groovy
@@ -39,60 +39,68 @@ WebDriver browser = ConfigTest.browser(config_browser, config_wait_time)
 Map<String, Object> credential = ConfigTest.credential()
 //println credential
 
-IndexPage index_page = new IndexPage(browser)
-index_page.load()
-
-// assert that the "Posts" header is displayed in the index page
-assert index_page.posts_header_exists()
-
-// register a new credential into the database
-// - open the Register page
-index_page.open_register_page()
-register_credential_page = new RegisterCredentialPage(browser)
-assert register_credential_page.register_button_exists()
-
-// - type username and password, then click the Register button
-register_credential_page.type_username(credential['username'])
-register_credential_page.type_password(credential['password'])
-register_credential_page.do_register()
-
-// - now we are transferred to the Login page
-login_page = new LogInPage(browser)
-assert login_page.login_button_exists()
-
-WebUI.delay(1)
-
-// Login with the added credential
-// - type credentials and do login
-login_page.type_username(credential['username'])
-login_page.type_password(credential['password'])
-login_page.do_login()
-
-// - make sure we are on the index page
-assert index_page.posts_header_exists()
-
-// create a new Post
-index_page.open_create_post_page()
-create_post_page = new CreatePostPage(browser)
-assert create_post_page.save_button_exists()
-
-// type title
-Map<String, String> song = Songs.song(0)
-String title = song.get('title')  + " --- " + song.get('by')
-create_post_page.type_title(title)
-// type body
-String lyric = song.get('lyric')
-create_post_page.type_body(lyric)
-// save the post
-create_post_page.do_save()
-
-// verify if the post is present in the index page
-Post post = index_page.get_post_latest()
-assert post != null
-assert post.get_title() == title
-assert post.get_body() == lyric
-
-WebUI.delay(1)
+step0(browser, credential)
 
 browser.quit()
+
+
+/*
+ * 
+ */
+def step0(WebDriver browser, credential) {
+	// let's start playing on the Flaskr web app
+	IndexPage index_page = new IndexPage(browser)
+	index_page.load()
+
+	// assert that the "Posts" header is displayed in the index page
+	assert index_page.posts_header_exists()
+
+	// register a new credential into the database
+	// - open the Register page
+	index_page.open_register_page()
+	RegisterCredentialPage register_credential_page = new RegisterCredentialPage(browser)
+	assert register_credential_page.register_button_exists()
+
+	// - type username and password, then click the Register button
+	register_credential_page.type_username(credential['username'])
+	register_credential_page.type_password(credential['password'])
+	register_credential_page.do_register()
+
+	// - now we are transferred to the Login page
+	LogInPage login_page = new LogInPage(browser)
+	assert login_page.login_button_exists()
+
+	WebUI.delay(1)
+
+	// Login with the added credential
+	// - type credentials and do login
+	login_page.type_username(credential['username'])
+	login_page.type_password(credential['password'])
+	login_page.do_login()
+
+	// - make sure we are on the index page
+	assert index_page.posts_header_exists()
+
+	// create a new Post
+	index_page.open_create_post_page()
+	CreatePostPage create_post_page = new CreatePostPage(browser)
+	assert create_post_page.save_button_exists()
+
+	// type title
+	Map<String, String> song = Songs.song(0)
+	String title = song.get('title')  + " --- " + song.get('by')
+	create_post_page.type_title(title)
+	// type body
+	String lyric = song.get('lyric')
+	create_post_page.type_body(lyric)
+	// save the post
+	create_post_page.do_save()
+
+	// verify if the post is present in the index page
+	Post post = index_page.get_post_latest()
+	assert post != null
+	assert post.get_title() == title
+	assert post.get_body() == lyric
+}
+
 
